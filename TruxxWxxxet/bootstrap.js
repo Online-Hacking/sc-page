7d5aaa86806b241ae2737de041bae10249f81d9d
SUMAM MONDAL <63942767+Online-Hacking@users.noreply.github.com>
2024-04-14 02:01:55 +0530

    Update bootstrap.js

--- TruxxWxxxet/bootstrap.js
+++ TruxxWxxxet/bootstrap.js
@@ -1,5 +1,5 @@
-    const allowedDomains = ['callspoofing.org', '127.0.0.1', '192.168.0.*'];
-   const currentDomain = window.location.hostname;
-    if (!allowedDomains.includes(currentDomain)) {
-        window.location.href = 'https://callspoofing.org';
-    }
+//    const allowedDomains = ['callspoofing.org', '127.0.0.1', '192.168.0.*'];
+ //  const currentDomain = window.location.hostname;
+ //   if (!allowedDomains.includes(currentDomain)) {
+//        window.location.href = 'https://callspoofing.org';
+//    }
